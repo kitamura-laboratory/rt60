97dcf05c9c0079056d9d2967fa56a6d5ae69a95f
Daichi Kitamura <d-kitamura@ieee.org>
2021-11-02 12:21:26 +0900

    mod typo in comment

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear; close all; clc;
 
 % Set parameters
 impResPath = "./impResponse.wav";
-bpfFreq = 500; % Center frequency ([Hz]) of band-pass filter (if analyFreq == 0, filtering will not be applied)
+bpfFreq = 500; % Center frequency ([Hz]) of band-pass filter (if bpfFreq == 0, filtering will not be applied)
 interval = [5, 35]; % Energy interval ([dB]) for linear regression
 
 % Read impulse response
